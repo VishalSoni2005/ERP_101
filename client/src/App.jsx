d3d26d2195544a31ddb891762a8ef3fb0b519985
VishalSoni007 <vsoni0882@gmail.com>
2025-02-10 17:36:02 +0530

    BD Added

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './Components/Header/Header.jsx';
+
+// import Login from '/src/Components/Login_Page/Login.jsx';
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <Login /> */}
+    </>
+  );
+}
+
+export default App;
